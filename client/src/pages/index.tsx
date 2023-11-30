4107c47d8d771e117995ed7359c5bfe5500ca72a
dlwnsghd00 <dlwnsghd00@gmail.com>
2023-11-30 12:07:49 +0900

    Community List Page Create

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,59 +1,116 @@
-import axios from 'axios';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/router';
-import React, { FormEvent, useState } from 'react'
-import InputGroup from '../components/InputGroup'
+import styles from '../styles/Home.module.css'
+import useSWR from 'swr';
+import { Post, Sub } from '../types'
+import axios from 'axios'
+import { useAuthState } from '../context/auth'
+import useSWRInfinite from 'swr/infinite';
+import { useEffect, useState } from 'react'
 
+const Home: NextPage = () => {
+  const { authenticated } = useAuthState();
 
-const Register = () => {
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState<any>({});
-    
+  const fetcher = async (url: string) => {
+    return await axios.get(url).then(res => res.data)
+  }
+  const address = "http://localhost:4000/api/subs/subs/topSubs";
 
-   
+  const getKey = (pageIndex: number, previousPageData: Post[]) => {
+    if (previousPageData && !previousPageData.length) return null;
+    return `/posts?page=${pageIndex}`;
+  }
 
-   
+  const { data, error, size: page, setSize: setPage, isValidating, mutate } = useSWRInfinite<Post[]>(getKey);
+  const isInitialLoading = !data && !error;
+  const posts: Post[] = data ? ([] as Post[]).concat(...data) : [];
+  const { data: topSubs } = useSWR<Sub[]>(address, fetcher)
 
-    return (
-        <div className='bg-white'>
-            <div className='flex flex-col items-center justify-center h-screen p-6'>
-                <div className='w-10/12 mx-auto md:w-96'>
-                    <h1 className='mb-2 text-lg font-medium'>회원가입</h1>
+  const [observedPost, setObservedPost] = useState("");
+
+  
+
+  const observeElement = (element: HTMLElement | null) => {
+    if (!element) return;
+    // 브라우저 뷰포트(ViewPort)와 설정한 요소(Element)의 교차점을 관찰
+    const observer = new IntersectionObserver(
+      // entries는 IntersectionObserverEntry 인스턴스의 배열
+      (entries) => {
+        // isIntersecting: 관찰 대상의 교차 상태(Boolean)
+        if (entries[0].isIntersecting === true) {
+          console.log("마지막 포스트에 왔습니다.");
+          setPage(page + 1);
+          observer.unobserve(element);
+        }
+      },
+      {threshold: 1}
+    );
+    // 대상 요소의 관찰을 시작
+    observer.observe(element);
+  }
+
+  return (
+    <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
+      {/* 포스트 리스트 */}
+      <div className='w-full md:mr-3 md:w-8/12'>
+      
+      
+
+      </div>
+
+      {/* 사이드바 */}
+      <div className='flex w-4/12 ml-3 md:block'>
+        <div className='bg-white border rounded'>
+          <div className='p-4 border-b'>
+            <p className='text-lg font-semibold text-center'>상위 커뮤니티</p>
+          </div>
+
+          {/* 커뮤니티 리스트 */}
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/r/${sub.name}`}>
                   
-                        <InputGroup
-                            placeholder='Email'
-                            value={email}
-                            setValue={setEmail}
-                            error={errors.email}
-                        />
-                        <InputGroup
-                            placeholder='Username'
-                            value={username}
-                            setValue={setUsername}
-                            error={errors.username}
-                        />
-                        <InputGroup
-                            placeholder='Password'
-                            value={password}
-                            setValue={setPassword}
-                            error={errors.password}
-                        />
-                        <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded'>
-                            회원 가입
-                        </button>
-                    
-                    <small>
-                        이미 가입하셨나요?
-                                
-                            <a className='ml-1 text-blue-500 uppercase'>로그인</a>
-                        
-                    </small>
-                </div>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                
+                </Link>
+                <Link href={`/r/${sub.name}`} className='ml-2 font-bold hover:cursor-pointer'>
+                    /r/{sub.name}
+                </Link>
+                <p className='ml-auto font-md'>{sub.postCount}</p>
+              </div>
+            ))}
+
+
+          </div>
+          {authenticated &&
+            <div className='w-full py-6 text-center'>
+              <Link href="/subs/create" className='w-full p-2 text-center text-white bg-gray-400 rounded'>
+                
+                  커뮤니티 만들기
+                
+              </Link>
             </div>
+          }
         </div>
-    )
+      </div>
+
+
+
+
+    </div>
+  )
 }
 
-export default Register
+export default Home
--- server/src/middlewares/user.ts
+++ server/src/middlewares/user.ts
@@ -15,7 +15,7 @@ try{
     console.log("user",user);
     if(!user) throw new Error("Unauthenticated")
     res.locals.user = user;
-
+    return next();
 } catch (error) {
     console.log(error);
     return res.status(400).json({error: "Something went wrong "});
